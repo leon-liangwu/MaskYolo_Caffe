7b011e66a06d57cc272c45db7bdaa1e4b694280f
leon-liangwu <wuliang0301@hotmail.com>
2019-07-19 00:57:38 +0800

    rm maskyolo dir

--- maskyolo/demos/demo_maskyolo.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.path.insert(0, '/home/wuliang/Workspace/caffe-maskYolo/python')
-sys.path.insert(0, '/home/wuliang/Workspace/cocoapi/PythonAPI/pycocotools')
-
-import caffe
-import numpy as np
-import cv2
-import random
-
